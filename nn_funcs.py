8d933f10dec6319612e57c22b1ee60b5e02bf020
Ilya Kuznetsov <ilyaprime26@yandex.ru>
2024-05-29 02:02:28 +0700

    Добавление описания к функциям в  nn_funcs.py

--- nn_funcs.py
+++ nn_funcs.py
@@ -1,15 +1,50 @@
 import numpy as np
 
-def relu(x):
+def relu(x: np.ndarray) -> np.ndarray:
+    """Функция активации ReLU.
+
+    Args:
+        x: Входной массив.
+
+    Returns:
+        Массив после применения ReLU.
+    """
     return np.maximum(0, x)
 
-def softmax(x):
+
+def softmax(x: np.ndarray) -> np.ndarray:
+    """Функция активации Softmax.
+
+    Args:
+        x: Входной массив.
+
+    Returns:
+        Массив после применения Softmax.
+    """
     return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
 
-def accuracy(y_pred, y_true):
+def accuracy(y_pred: np.ndarray, y_true: np.ndarray) -> float:
+    """Функция вычисления точности.
+
+    Args:
+        y_pred: Предсказанные метки.
+        y_true: Истинные метки.
+
+    Returns:
+        Значение точности.
+    """
     return np.mean(np.argmax(y_pred, axis=1) == y_true)
 
-def cross_entropy_loss(y_pred, y_true):
+def cross_entropy_loss(y_pred: np.ndarray, y_true: np.ndarray) -> float:
+    """Функция вычисления потерь кросс-энтропии.
+
+    Args:
+        y_pred: Предсказанные вероятности.
+        y_true: Истинные метки.
+
+    Returns:
+        Значение потерь.
+    """
     n_samples = y_pred.shape[0]
     y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
     if len(y_true.shape) == 1:
