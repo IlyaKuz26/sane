6efc24c5f9ba88839b0ec6a3aa955cc4960b4d2f
Ilya Kuznetsov <ilyaprime26@yandex.ru>
2024-05-28 22:55:17 +0700

    Добавление в nn_funcs функции для вычисления потерь кросс-энтропии

--- nn_funcs.py
+++ nn_funcs.py
@@ -8,3 +8,14 @@ def softmax(x):
 
 def accuracy(y_pred, y_true):
     return np.mean(np.argmax(y_pred, axis=1) == y_true)
+
+def cross_entropy_loss(y_pred, y_true):
+    n_samples = y_pred.shape[0]
+    y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+    if len(y_true.shape) == 1:
+        confidences = y_pred_clipped[range(n_samples), y_true]
+    elif len(y_true.shape) == 2:
+        confidences = np.sum(y_pred_clipped * y_true, axis=1)
+
+    negative_log_likehoods = -np.log(confidences)
+    return np.mean(negative_log_likehoods)
